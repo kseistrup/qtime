b106c3ae44cee3c8bd897fa420f503dadb62ac63
Klaus Alexander Seistrup <klaus@seistrup.dk>
2017-03-27 16:19:39 +0200

    Initial commit

---
+++ lisp/qtime.lisp
@@ -0,0 +1,63 @@
+#!/usr/bin/env clisp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Copyright ©2017 Klaus Alexander Seistrup <klaus@seistrup.dk>
+;;
+;; Version: 0.1.0 (2017-03-27)
+;;
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but with-
+;; out any warranty; without even the implied warranty of merchantability or
+;; fitness for a particular purpose. See the GNU General Public License for
+;; more details. <http://gplv3.fsf.org/>
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; qtime
+(defun qtime ()
+   "Returns current local time as an English sentence"
+   (setq adjust
+      (+ (truncate
+            (+ (rem
+                  (multiple-value-bind
+                     (ss mm hh dd mmm yyyy dow dst-p tz)
+                     (get-decoded-time)
+                     (+ (* (+ (* hh 60) mm) 60) ss))
+                  (* 60 60 24))
+               30) 60) 27))
+
+   (setq minutes
+      (rem adjust 60))
+   (setq divisions
+      (- (truncate minutes 5) 5))
+   (setq hours
+      (truncate adjust 60))
+   (setq hours12
+      (rem hours 12))
+
+   (setq elms
+      (list
+         (nth (rem minutes 5)
+            '("nearly " "almost " "" "just after " "after "))
+         (nth (abs divisions)
+            '("" "five " "ten " "a quarter "
+                "twenty " "twenty-five " "half "))
+         (cond
+            ((< divisions 0) "to ")
+            ((> divisions 0) "past ")
+            (""))
+         (cond
+            ((= hours 0) "midnight")
+            ((= hours 12) "noon")
+            ((format nil "~r" hours12)))))
+
+   (format nil "~{~A~}"
+      (if (and (= divisions 0) (not (= hours12 0)))
+         (append elms '(" o'clock")) elms)))
+
+;;; Do you have the time?
+(princ (format nil "It's ~A." (qtime)))
+
+;;;; eof
