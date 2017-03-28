65a7145cdfd5713955be04fc47811671266e1831
Klaus Alexander Seistrup <klaus@seistrup.dk>
2017-03-28 15:08:29 +0200

    indent -gnu -npcs -i4

--- c/qtime.c
+++ c/qtime.c
@@ -28,69 +28,62 @@
 #include <stdio.h>
 #include <time.h>
 
-/**/
 static long
-get_secs (void)
+get_secs(void)
 {
-  time_t sys_time = time (NULL);
-  struct tm *sys_date = localtime (&sys_time);
+    time_t sys_time = time(NULL);
+    struct tm *sys_date = localtime(&sys_time);
 
-  return ((sys_date->tm_hour * 60L) + sys_date->tm_min) * 60L + sys_date->tm_sec;
+    return ((sys_date->tm_hour * 60L) + sys_date->tm_min) * 60L +
+	sys_date->tm_sec;
 }
 
-/**/
 static void
-qt (void)
+qt(void)
 {
-  static char *const hr[] =
-  {
-    "twelve", "one", "two", "three", "four", "five",
-    "six", "seven", "eight", "nine", "ten", "eleven"
-  };
-  static char *const mn[] =
-  {
-    "", "five ", "ten ", "a quarter ",
-    "twenty ", "twenty-five ", "half "
-  };
-  static char *const ny[] =
-  {
-    "nearly ", "almost ", "", "just after ", "after "
-  };
-  static char *const up[] =
-  {
-    "to ", "", "past "
-  };
+    static char *const hr[] = {
+	"twelve", "one", "two", "three", "four", "five",
+	"six", "seven", "eight", "nine", "ten", "eleven"
+    };
+    static char *const mn[] = {
+	"", "five ", "ten ", "a quarter ",
+	"twenty ", "twenty-five ", "half "
+    };
+    static char *const ny[] = {
+	"nearly ", "almost ", "", "just after ", "after "
+    };
+    static char *const up[] = {
+	"to ", "", "past "
+    };
 
-  long adj_mins	   = (get_secs () + 30L) / 60L + 27L;
-  long hours	   = (adj_mins / 60L) % 12L;
-  long minutes	   =  adj_mins % 60L;
-  long almost	   =  minutes % 5L;
-  long divisions   = (minutes / 5L) - 5L;
-  long to_past_idx = divisions > 0L ? 1L : 0L;
+    long adj_mins = (get_secs() + 30L) / 60L + 27L;
+    long hours = (adj_mins / 60L) % 12L;
+    long minutes = adj_mins % 60L;
+    long almost = minutes % 5L;
+    long divisions = (minutes / 5L) - 5L;
+    long to_past_idx = divisions > 0L ? 1L : 0L;
 
-  if (divisions < 0L)
-    {
-      divisions = -divisions;
-      to_past_idx = -1L;
-    }
-  ++to_past_idx;
+    if (divisions < 0L)
+      {
+	  divisions = -divisions;
+	  to_past_idx = -1L;
+      }
+    ++to_past_idx;
 
-  (void) printf ("It's %s%s%s%s",
-		 ny[almost], mn[divisions],
-		 up[to_past_idx], hr[hours]);
+    (void) printf("It's %s%s%s%s",
+		  ny[almost], mn[divisions], up[to_past_idx], hr[hours]);
 
-  if (divisions == 0L)
-    (void) puts (" o'clock.");
-  else
-    (void) puts (".");
+    if (divisions == 0L)
+	(void) puts(" o'clock.");
+    else
+	(void) puts(".");
 }
 
-/**/
 int
-main (int argc, char *const *argv)
+main(int argc, char *const *argv)
 {
-  qt ();
-  return 0;
+    qt();
+    return 0;
 }
 
 /*
